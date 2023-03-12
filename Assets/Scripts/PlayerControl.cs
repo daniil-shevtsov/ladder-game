d8ad5e31bd98b2c2504f20281f302275ca1910c5
Daniil Shevtsov <me@daniilshevtsov.com>
2023-03-12 18:00:56 +0400

    add debug option

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,6 +7,9 @@ public class PlayerControl : MonoBehaviour
 
     public Transform hammerHead;
     public Transform body;
+    public Vector3 hammerRotation;
+    public Vector3 hammerPosition;
+
     private Transform originalHammerTransform;
     private Quaternion originalRotation;
 
@@ -40,53 +43,17 @@ public class PlayerControl : MonoBehaviour
         Vector3 mouseVec = Vector3.ClampMagnitude(mouse - center, maxRange);
         Vector3 desiredHammerDirection = mouse - transform.position;
 
-        // Collider[] results = Physics.OverlapSphere(hammerHead.transform.position, 0.3f);
-        // if(results.Length > 0)
-        // {
-        //     // Update body pos
-        //     Vector3 targetBodyPos = mouse - hammerHead.position;
-        //     Debug.DrawLine(hammerHead.position, targetBodyPos, Color.yellow, 1);
-
-        //     Vector3 force = (targetBodyPos - body.position) * 80.0f;
-        //     body.GetComponent<Rigidbody>().AddForce(force);
-
-        //     body.GetComponent<Rigidbody>().velocity = Vector2.ClampMagnitude(
-        //         body.GetComponent<Rigidbody>().velocity, 6);
-        // }
-
         // Compute new hammer pos
         Vector3 newHammerPos = body.position + desiredHammerDirection;
         Vector3 hammerMoveVec = newHammerPos - hammerHead.position;
-        newHammerPos = hammerHead.position + hammerMoveVec * 0.2f;
+        newHammerPos = hammerPosition; //hammerHead.position + hammerMoveVec * 0.2f;
+        Vector3 newHammerRotation = hammerRotation;
 
         // Update hammer pos
-        hammerHead.GetComponent<Rigidbody>().MovePosition(newHammerPos);
-        hammerHead.transform.position = originalHammerTransform.position;
-        hammerHead.transform.rotation = originalRotation;
-        // Update hammer rotation
-        //hammerHead.rotation = Quaternion.FromToRotation( Vector3.forward, newHammerPos - body.position);
+        //hammerHead.GetComponent<Rigidbody>().MovePosition(newHammerPos);
 
+        hammerHead.transform.position = hammerPosition;//originalHammerTransform.position;
+        hammerHead.transform.localEulerAngles = hammerRotation;//originalRotation;
 
-
-
-
-
-
-
-
-        //hammerHead.transform.LookAt(body.position);
-        //hammerHead.transform.LookAt(body.position, -Vector3.forward);
-        //body.transform.LookAt(hammerHead.position, -Vector3.right);
-        // Vector3 direction = body.position - hammerHead.position;
-        // //body.rotation = Quaternion.LookRotation(direction,Vector3.up);
-        // //hammerHead.transform.RotateAround(body.position,Vector3.up, 0);
-
-        // // hammerHead.forward = body.forward;
-
-        // hammerHead.eulerAngles =  new Vector3(
-        //     body.eulerAngles.x - 90,
-        //     body.eulerAngles.y,
-        //     body.eulerAngles.z
-        // );
     }
 }
