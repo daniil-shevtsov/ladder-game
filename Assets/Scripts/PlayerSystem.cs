62b112fdde9cdf69ad77bca7a924202f7751a244
Daniil Shevtsov <me@daniilshevtsov.com>
2023-04-08 17:29:57 +0400

    start TDDing the movement

---
+++ Assets/Scripts/PlayerSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 onMoveInput(
+        float vertical,
+        float horizontal
+    )
+    {
+        return new Vector3(0f,0f,0f);
+    }
+}
--- Assets/Tests/PlayMode/PlayerTest.cs
+++ Assets/Tests/PlayMode/PlayerTest.cs
@@ -8,25 +8,28 @@ public class PlayerTest : MonoBehaviour
 {
     // private ButtonController controller;
     // private TMPro.TMP_Text result;
+    private GameObject player;
+    private PlayerSystem playerSystem;
 
     [OneTimeSetUp]
     public void Setup() {
+        player = MonoBehaviour.Instantiate (
+            Resources.Load<GameObject>("Prefabs/Body"),
+            new Vector3( 0.0f, 0.0f, 1.0f),
+            Quaternion.identity
+        );
+        playerSystem = player.GetComponent<PlayerSystem>();
         
     }
 
 
     // A Test behaves as an ordinary method
     [UnityTest]
-    public IEnumerator ButtonControllerTestSimplePasses()
+    public IEnumerator ShouldStayWhenZeroInput()
     {
-        GameObject playerGameObject = MonoBehaviour.Instantiate (
-            Resources.Load<GameObject>("Prefabs/Body"),
-            new Vector3( 0.0f, 0.0f, 1.0f),
-            Quaternion.identity
-        );
-        
+        var movement = playerSystem.onMoveInput(0f, 0f);
+        Assert.AreEqual(movement, Vector3.zero);
         yield return new WaitForSeconds(0.1f);
-        
     }
 
 
